8338efbe87d0ac9c05e9c26b0f53d0bd79cf16d2
Alvaro Martinez <martinez.alvaro@correounivalle.edu.co>
2017-07-20 01:32:12 -0500

    Pipes json, currency, number exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,17 @@ export class AppComponent {
   nombre = "Alvaro";
   title = 'app';
   arreglo = [1,2,3,4,5,6,7,8,9,10];
+  PI = Math.PI;
+  percentNumber = 0.234;
+  salario = 1234.5;
+  heroe = {
+    nombre : 'Logan',
+    clave : 'Wolverine',
+    edad: 500,
+    direccion : {
+      calle: "Primera",
+      casa: '32'
+    }
+  };
 
 }
